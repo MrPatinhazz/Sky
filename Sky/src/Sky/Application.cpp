a7d37b05638162f7bffd17433255eda3d2829e39
Guilherme Gomes <jguilhermefgomes@gmail.com>
2020-10-01 18:20:21 +0100

    Handle Window Events

--- Sky/src/Sky/Application.cpp
+++ Sky/src/Sky/Application.cpp
@@ -1,16 +1,17 @@
 #include "skypch.h"
 #include "Application.h"
-
-#include "Sky/Events/ApplicationEvent.h"
 #include "Sky/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Sky {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
@@ -25,5 +26,18 @@ namespace Sky {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		SKY_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Sky/src/Sky/Application.h
+++ Sky/src/Sky/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Sky/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Sky {
@@ -14,7 +16,11 @@ namespace Sky {
 
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
